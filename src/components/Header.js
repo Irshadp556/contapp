450b94b6077873325bb70055bf999c81be4e6876
Irshadp556 <pirshad443@gmail.com>
2024-02-03 10:58:06 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu" style={{backgroundColor:"#808080"}}>
+      <div className="ui container center">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
